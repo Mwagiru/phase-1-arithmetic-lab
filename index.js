ff7496c8cf2e5be4efd29a7b4932a9a482d8f4ca
Mwagiru <joymwagiru@gmail.com>
2022-04-06 11:07:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 =31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+/////////////////
+let random =Math.floor(Math.random()*12);
+console.log(random);
+////////////////
+let num3 = 22;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+////////////////
+let max  = Math.max(13, 8, 3, 20, 15);
+console.log(max);
